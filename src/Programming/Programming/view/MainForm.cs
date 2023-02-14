5fae7a3ce6f0177b35216ae5ec3161bee3d75a25
umrualone <umrualone@gmail.com>
2023-02-14 17:36:16 +0700

    Add Windows Form

---
+++ src/Programming/Programming/view/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Programming
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}

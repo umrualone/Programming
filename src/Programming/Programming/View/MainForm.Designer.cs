a47698af4201af4390e29ff7d929071b8bf71e0f
umrualone <umrualone@gmail.com>
2023-02-14 19:12:10 +0700

    Design layout and fulfillment of points 10-12

---
+++ src/Programming/Programming/View/MainForm.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace Programming
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageEnums = new System.Windows.Forms.TabPage();
+            this.groupBoxEnumerations = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelIndex = new System.Windows.Forms.Label();
+            this.textBoxValue = new System.Windows.Forms.TextBox();
+            this.ValuesListBox = new System.Windows.Forms.ListBox();
+            this.EnumsListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPageEnums.SuspendLayout();
+            this.groupBoxEnumerations.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPageEnums);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(799, 459);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPageEnums
+            // 
+            this.tabPageEnums.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tabPageEnums.Controls.Add(this.groupBoxEnumerations);
+            this.tabPageEnums.Location = new System.Drawing.Point(4, 22);
+            this.tabPageEnums.Name = "tabPageEnums";
+            this.tabPageEnums.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageEnums.Size = new System.Drawing.Size(791, 433);
+            this.tabPageEnums.TabIndex = 0;
+            this.tabPageEnums.Text = "Enums";
+            this.tabPageEnums.UseVisualStyleBackColor = true;
+            // 
+            // groupBoxEnumerations
+            // 
+            this.groupBoxEnumerations.Controls.Add(this.label2);
+            this.groupBoxEnumerations.Controls.Add(this.label1);
+            this.groupBoxEnumerations.Controls.Add(this.labelIndex);
+            this.groupBoxEnumerations.Controls.Add(this.textBoxValue);
+            this.groupBoxEnumerations.Controls.Add(this.ValuesListBox);
+            this.groupBoxEnumerations.Controls.Add(this.EnumsListBox);
+            this.groupBoxEnumerations.Location = new System.Drawing.Point(6, 1);
+            this.groupBoxEnumerations.Name = "groupBoxEnumerations";
+            this.groupBoxEnumerations.Size = new System.Drawing.Size(776, 413);
+            this.groupBoxEnumerations.TabIndex = 2;
+            this.groupBoxEnumerations.TabStop = false;
+            this.groupBoxEnumerations.Text = "Enumerations";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(206, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 14);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Choose value:";
+            // 
+            // labelIndex
+            // 
+            this.labelIndex.Location = new System.Drawing.Point(377, 25);
+            this.labelIndex.Name = "labelIndex";
+            this.labelIndex.Size = new System.Drawing.Size(55, 14);
+            this.labelIndex.TabIndex = 3;
+            this.labelIndex.Text = "Int value:";
+            // 
+            // textBoxValue
+            // 
+            this.textBoxValue.Location = new System.Drawing.Point(377, 42);
+            this.textBoxValue.Name = "textBoxValue";
+            this.textBoxValue.Size = new System.Drawing.Size(126, 20);
+            this.textBoxValue.TabIndex = 2;
+            // 
+            // ValuesListBox
+            // 
+            this.ValuesListBox.FormattingEnabled = true;
+            this.ValuesListBox.Location = new System.Drawing.Point(206, 42);
+            this.ValuesListBox.Name = "ValuesListBox";
+            this.ValuesListBox.Size = new System.Drawing.Size(152, 225);
+            this.ValuesListBox.TabIndex = 1;
+            this.ValuesListBox.SelectedIndexChanged += new System.EventHandler(this.ValuesListBox_SelectedIndexChanged);
+            // 
+            // EnumsListBox
+            // 
+            this.EnumsListBox.FormattingEnabled = true;
+            this.EnumsListBox.Location = new System.Drawing.Point(27, 42);
+            this.EnumsListBox.Name = "EnumsListBox";
+            this.EnumsListBox.Size = new System.Drawing.Size(152, 225);
+            this.EnumsListBox.TabIndex = 0;
+            this.EnumsListBox.SelectedIndexChanged += new System.EventHandler(this.EnumsListBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(27, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Choose enumaration:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 459);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageEnums.ResumeLayout(false);
+            this.groupBoxEnumerations.ResumeLayout(false);
+            this.groupBoxEnumerations.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Label labelIndex;
+
+        private System.Windows.Forms.TextBox textBoxValue;
+
+        private System.Windows.Forms.GroupBox groupBoxEnumerations;
+
+        private System.Windows.Forms.ListBox ValuesListBox;
+
+        private System.Windows.Forms.ListBox EnumsListBox;
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageEnums;
+
+        #endregion
+    }
+}
---
+++ src/Programming/Programming/View/MainForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Programming
+{
+    public partial class MainForm : Form
+    {
+        
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            object[] enums = new object[] { typeof(Color), typeof(Genre), typeof(EducationForm), typeof(Manufactures), typeof(Season), typeof(Weekday) };
+            EnumsListBox.Items.AddRange(enums);
+        }
+        
+        private void EnumsListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (EnumsListBox.SelectedItem == null)
+            {
+                return;
+            }
+
+            var selectedEnum = (Type)EnumsListBox.SelectedItem;
+            var enumValues = Enum.GetValues(selectedEnum);
+            ValuesListBox.Items.Clear();
+            foreach (var enumValue in enumValues)
+            {
+                ValuesListBox.Items.Add(enumValue);
+            }
+        }
+
+        private void ValuesListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBoxValue.Text = ValuesListBox.SelectedIndex.ToString();
+        }
+    }
+}

74511de680ee0a13c5a6cd7074c81fa20a33e504
Andrey Kotelnikov <umrualone@gmail.com>
2023-05-13 18:47:03 +0700

    Init Notes App

---
+++ src/NotesApp/NotesApp/View/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace NotesApp.View
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
